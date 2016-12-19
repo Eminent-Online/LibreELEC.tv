EmuELEC/Sx05RE

libcec: check TARGET_KERNEL_ARCH when deciding on Amlogic implementation

packages/devel/libcec/package.mk
@@ -48,7 +48,7 @@ else
 fi
 
 if [ "$KODIPLAYER_DRIVER" = "libamcodec" ]; then
-  if [ "$TARGET_ARCH" = "aarch64" ]; then
+  if [ "$TARGET_KERNEL_ARCH" = "arm64" ]; then
     PKG_CMAKE_OPTS_TARGET="$PKG_CMAKE_OPTS_TARGET -DHAVE_AOCEC_API=1"
   else
     PKG_CMAKE_OPTS_TARGET="$PKG_CMAKE_OPTS_TARGET -DHAVE_AMLOGIC_API=1"

Eminent-Online/LibreELEC.tv

packages/devel/libcec/package.mk
@@ -44,9 +44,9 @@ else
 fi
 
 if [ "$KODIPLAYER_DRIVER" = "libamcodec" ]; then
-  if [ "$TARGET_ARCH" = "arm" ]; then
+  if [ "$TARGET_KERNEL_ARCH" = "arm" ]; then
     EXTRA_CMAKE_OPTS="$EXTRA_CMAKE_OPTS -DHAVE_AOCEC_API=0 -DHAVE_AMLOGIC_API=1"
-  elif [ "$TARGET_ARCH" = "aarch64" ]; then
+  elif [ "$TARGET_KERNEL_ARCH" = "arm64" ]; then
     EXTRA_CMAKE_OPTS="$EXTRA_CMAKE_OPTS -DHAVE_AOCEC_API=1 -DHAVE_AMLOGIC_API=0"
   fi
 else