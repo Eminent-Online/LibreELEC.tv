5d803243373a4b6d921b1e5fd3b29dd2ec0a6e56
Eminent <info@eminent-online.com>
2017-03-11 12:34:29 -0500

    kodi: update version to c6d0bb5

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="kodi"
-PKG_VERSION="fd49f98"
+PKG_VERSION="c6d0bb5"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
